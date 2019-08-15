b7e9710f3f2b6a935629946e7dd2f598051db832
bbarres <benoit.barres@gmail.com>
2019-08-15 02:45:18 +0200

    more cleaning and reorganization

--- fitness_analysis.R
+++ fitness_analysis.R
@@ -2,21 +2,28 @@
 ###############################################################################
 #Fitness related traits
 ###############################################################################
+###############################################################################
 
-## EGG TO ADULT ############
+source("load_bemisia_data.R")
+
+
+###############################################################################
+#Egg to adult
+###############################################################################
 
 #building the 'fitness' variable
 larvae$fitness<-larvae$euf.tot*larvae$X.emergence
 
-
 ## models with DL50 effect
-mod1 <- glm(X.eclosion~DLpop, family=quasibinomial(link='logit'), weights=oeufs, data=larvae)
+mod1 <- glm(X.eclosion~DLpop, family=quasibinomial(link='logit'), 
+            weights=oeufs, data=larvae)
 summary(mod1)
 # overdispersion = 5.5
 anova(mod1, test="F")
 plot(larvae$X.eclosion~larvae$DLpop)
 
-mod2 <- glm(X.emergence~DLpop, family=quasibinomial(link='logit'), weights=oeufs, data=larvae)
+mod2 <- glm(X.emergence~DLpop, family=quasibinomial(link='logit'), 
+            weights=oeufs, data=larvae)
 summary(mod2)
 # overdispersion = 5.3
 anova(mod2, test="F")
@@ -26,13 +33,15 @@ summary(mod3)
 anova(mod3, test="F")
 
 ## models with population effect
-mod4 <- glm(X.eclosion~pop, family=quasibinomial(link='logit'), weights=oeufs, data=larvae)
+mod4 <- glm(X.eclosion~pop, family=quasibinomial(link='logit'), 
+            weights=oeufs, data=larvae)
 summary(mod4)
 # overdispersion = 5.1
 anova(mod4, test="F")
 summary(glht(mod4,linfct = mcp(pop="Tukey")))
 
-mod5 <- glm(X.emergence~pop, family=quasibinomial(link='logit'), weights=oeufs, data=larvae)
+mod5 <- glm(X.emergence~pop, family=quasibinomial(link='logit'), 
+            weights=oeufs, data=larvae)
 summary(mod5)
 # overdispersion = 5.1
 anova(mod5, test="F")
@@ -43,10 +52,9 @@ summary(mod6)
 anova(mod6, test="F")
 
 
-## FEMALE TRAITS #############
-
-# data
-clip <- read.delim("traits-female.txt",dec=",")
+###############################################################################
+#Female traits
+###############################################################################
 
 ## models with DL50 effect
 mod1 <- glm(euf.tot~DLpop, family="quasipoisson", data=clip)
@@ -73,10 +81,9 @@ anova(mod4, test="Chisq")
 summary(glht(mod4,linfct = mcp(pop="Tukey")))
 
 
-## FEMALE SIZE #############
-
-# data
-sizef <- read.delim("female-size.txt", dec=",")
+###############################################################################
+#Female size
+###############################################################################
 
 ## models
 mod <- lm(taille~pop, data=sizef)
@@ -86,3 +93,8 @@ summary(mod)
 mod1 <- lm(taille~DL50, data=sizef)
 summary(mod1)
 anova(mod1)
+
+
+###############################################################################
+#END
+###############################################################################
--- load_bemisia_data.R
+++ load_bemisia_data.R
@@ -13,15 +13,24 @@ library(multcomp)
 #loading the results of the insecticide bioassays
 ###############################################################################
 
+# data importation
+gamme<-read.delim("data/insecticides-mortality.txt",header=TRUE,sep="\t")
+
+# mortalities
+gamme$Total<-gamme$Eff.morts + gamme$Eff.vivants
+
+mortalites<-aggregate(gamme[,c("Eff.morts","Total")],
+                      list(Population=gamme$Population,
+                           Produit=gamme$Produit,
+                           Concentration=gamme$Concentration),sum)
+
+mortalites$Morta.Pool<-mortalites$Eff.morts/mortalites$Total
+
+summary(gamme)
+summary(mortalites)
 
 
-
-
-
-
-
-
-
+environ<-read.table("data/environment.txt", header=TRUE, sep="\t")
 
 
 ###############################################################################
@@ -31,19 +40,11 @@ library(multcomp)
 #loading the egg to larvae dataset
 larvae<-read.table("data/traits-egg-larvae.txt",header=TRUE,sep="\t")
 
+#loading the female traits dataset
+clip<-read.table("data/traits-female.txt",header=TRUE,sep="\t")
 
-
-
-
-
-
-
-
-
-
-
-
-
+#loading the female size dataset
+sizef<-read.table("data/female-size.txt",header=TRUE,sep="\t")
 
 
 
