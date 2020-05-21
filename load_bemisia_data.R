059e2418fa303d5d1d5290f7e0b69b78bcef9b74
bbarres <benoit.barres@gmail.com>
2020-05-21 17:56:31 +0200

    adding session information for reproducibility

--- load_bemisia_data.R
+++ load_bemisia_data.R
@@ -43,5 +43,16 @@ sizef<-read.table("data/female-size.txt",header=TRUE,sep="\t")
 
 
 ##############################################################################/
+#Writing info session for reproducibility####
+##############################################################################/
+
+sink("session_info.txt")
+print(sessioninfo::session_info())
+sink()
+#inspired by an R gist of François Briatte: 
+#https://gist.github.com/briatte/14e47fb0cfb8801f25c889edea3fcd9b
+
+
+##############################################################################/
 #END
 ##############################################################################/
